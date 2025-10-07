c25688012547e84b425a31f7c6928cd8677137fe
betapcode17 <quocdat19991712@gmail.com>
2025-10-07 09:13:15 +0700

    done componet header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="space-y-2 text-center">
+      <h1 className="text-4xl font-bold text-transparent bg-primary bg-clip-text">
+        TodoX
+      </h1>
+      <p className="text-muted-foreground">
+        どんな困難なことも、乗り越えられないことはない。恐れるべきは、やらないことだ。
+      </p>
+    </div>
+  );
 };
 
 export default Header;
