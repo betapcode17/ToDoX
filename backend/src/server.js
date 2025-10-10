e746541db8be17752936b87f2ba5d4a8f902d9e0
betapcode17 <quocdat19991712@gmail.com>
2025-10-10 17:02:12 +0700

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,30 @@ import TaskRoute from "./routes/tasksRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+// Tự xác định vị trí đang chạy mã
+const __dirname = path.resolve();
 // dung server ctrl + c
 const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
 
 app.use("/api/tasks", TaskRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
